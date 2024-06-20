19585403b11a2b4708bdf0ce0ee9dc91da719f7a
Antoine Magnin <ant.mag@icloud.com>
2024-06-20 19:30:12 +0200

    fix: remove mapstruct warning during build process

--- src/main/java/io/kukua/springbootapi/user/dto/UserMapper.java
+++ src/main/java/io/kukua/springbootapi/user/dto/UserMapper.java
@@ -3,8 +3,9 @@ package io.kukua.springbootapi.user.dto;
 import io.kukua.springbootapi.auth.dto.request.RegisterRequest;
 import io.kukua.springbootapi.user.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
     User fromDto(RegisterRequest registerRequest);
