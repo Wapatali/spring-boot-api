0102be97caa3d77900b91f9ee1a40d6209d7b09a
Antoine Magnin <ant.mag@icloud.com>
2024-09-10 15:49:43 +0200

    fix(user): make update endpoint transactional

--- src/main/java/io/kukua/springbootapi/user/UserController.java
+++ src/main/java/io/kukua/springbootapi/user/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -37,6 +38,7 @@ public class UserController {
         return ResponseEntity.ok(users);
     }
 
+    @Transactional
     @PutMapping("/{id}")
     @Secured({"ROLE_USER"})
     public ResponseEntity<?> update(@RequestBody UpdateRequest updateRequest,
